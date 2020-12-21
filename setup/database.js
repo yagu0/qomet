26325ff0a1789ee176f74f070059668b66aa0ad1
Benjamin Auder <benjamin.auder@somewhere>
2020-12-21 16:07:29 +0100

    Attempt to resurrect qomet code - need some rewrite

--- setup/database.js
+++ setup/database.js
@@ -1,13 +1,7 @@
-// TODO: createCollections users, courses, assessments
-// with:
-// users
-//   unique initials, email
-//   index initials, email
-// courses
-//   unique (code,uid)
-//   index (code,uid)
-// evaluations
-//   unique (cid, name)
-//   index (cid, name)
-// db.evaluations.createIndex( { cid: 1, name: 1 } );
-// https://docs.mongodb.com/manual/core/index-compound/
+use qomet
+db.createCollection("users")
+db.createCollection("courses")
+db.createCollection("evaluations")
+db.users.createIndex({ initials: 1, email: 1 }, { unique: true } )
+db.courses.createIndex({ code: 1, uid: 1 }, { unique: true } )
+db.evaluations.createIndex({ cid: 1, name: 1 }, { unique: true } )
